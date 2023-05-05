349756b046476855dbd9f66c841662cb1d3b2ad8
Alber0087 <albertorg0087@gmail.com>
2023-05-05 15:39:46 +0100

    Iterations 1 & 2 done

---
+++ index.php
@@ -0,0 +1,33 @@
+<?php
+$title      = 'Wiitube';
+$copyright  = '© Alberto Rodríguez 2024';
+
+
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+  <head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="styles.css">
+    <title><?php echo $title; ?></title>
+  </head>
+  <body>
+    <header class="header">
+      <h1><?php echo $title; ?></h1>
+      <button class="loginButton">Login</button>
+    </header>
+    <main class="main">
+      <h2>HOME</h2>
+    </main>
+    <footer class="footer">
+      <p><?php echo $copyright; ?></p>
+    </footer>
+    
+  </body>
+</html>
+
+
+
